715a41eaaf07ef0ca8338c23b45c612b36c58321
Stan Wielga <svwielga4@gmail.com>
2020-09-24 19:14:28 -0400

    adding socket support

---
+++ socket_gallows/config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :socket_gallows, SocketGallowsWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "Dpd25IYDJIPZw2rR5wxvsVQPElDr/ztirXCg2/230E40IPW4R6B9A7qWeK+aA6lL",
+  render_errors: [view: SocketGallowsWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: SocketGallows.PubSub,
+  live_view: [signing_salt: "Mxw5hdc2"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
