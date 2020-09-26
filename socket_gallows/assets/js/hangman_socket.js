31456dcc31b17970a9c6a90dd3a503cdbd65045d
Stan Wielga <svwielga4@gmail.com>
2020-09-26 12:48:09 -0400

    connects to channel

--- socket_gallows/assets/js/hangman_socket.js
+++ socket_gallows/assets/js/hangman_socket.js
@@ -5,4 +5,9 @@ export default class HangmanSocket {
         this.socket = new Socket("/socket", {});
         this.socket.connect();
     }
+
+    connect_to_hangman() {
+        this.channel = this.socket.channel("hangman:game", {});
+        this.channel.join();
+    }
 }
